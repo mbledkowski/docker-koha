cb9e9fe2ffdb9fbbc01f9f2f14b20cc355cf0f2b
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-15 14:58:10 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,13 +10,16 @@
  echo "GRANT ALL ON drupal.* TO kohaadmin@localhost IDENTIFIED BY 'kohadbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
  
  cd /opt
+ wget 
+ 
+ cd /opt
  wget http://archive.apache.org/dist/lucene/solr/4.9.1/solr-4.9.1.tgz
  tar -xvf solr-4.9.1.tgz
  rm solr-4.9.1.tgz
  cd solr-4.9.1/
  cp -a example koha
  rm -rf koha/solr/conf
- ln -s $KOHA_BASE/etc/solr/conf ./koha/solr/conf
+ ln -s /opt/koha/etc/solr/conf ./koha/solr/conf
 
 
 killall mysqld
