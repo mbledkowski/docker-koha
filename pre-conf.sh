1d5bd2248bbe3c1a6e4511dc8b91e728325e1045
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-17 22:54:15 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -3,23 +3,23 @@
 /usr/bin/mysqld_safe &
  sleep 10s
 
-
- 
  #need to edit or add file /etc/koha/koha-sites.conf
  a2enmod rewrite
  a2enmod cgi
  koha-create --create-db library
+
  #security tweak
   mysqladmin -u root password mysqlpsswd
   mysqladmin -u root -pmysqlpsswd reload
+
  #apache2 conf
  #default ports /etc/apache2/ports.conf   need to add Listen 80  and 8080
+
  a2enmod deflate
  a2ensite library
+
  #tweat /etc/hosts if nesesary ....
- #need to be added after_install scritp to get password of default user koha_library
- #xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/library/koha-conf.xml
- 
+
  #need to run only afte upgrade 
  #koha-rebuild-zebra -v -f library
  
