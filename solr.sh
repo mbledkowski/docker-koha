c42d76b59a1f03d6439dfaa8eccb6d466fc32a6e
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-17 21:07:09 -0500

    Delete solr.sh

--- solr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-### In solr.sh (make sure this file is chmod +x):
-# `/sbin/setuser mysql` runs the given command as the user `mysql`.
-# If you omit that part, the command will be run as root.
-
-sleep 15
-cd /opt/solr-4.9.1/koha/
-exec  java -jar start.jar >>/var/log/solr.log 2>&1
