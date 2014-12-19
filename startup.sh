1739c00a176cf099083ee5e090b5411bd904ff61
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-19 04:45:45 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
       #code that need to run only one time ....
-        
+        xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/library/koha-conf.xml
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
