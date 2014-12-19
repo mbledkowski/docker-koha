b705e21bac74c5be1baf271edb7b629e4fad8c36
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-19 16:01:06 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
       #code that need to run only one time ....
+        a2enmod suexec
         xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/library/koha-conf.xml
         #a2enmod suexec
         #needed for fix problem with ubuntu and cron
