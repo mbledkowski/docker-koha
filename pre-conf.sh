c6b14d9cd5680e61d3cfca62b16a5fab20789659
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-18 22:18:57 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 /usr/bin/mysqld_safe &
- sleep 10s
+ sleep 5s
+
+ #apache2 conf
+ #default ports /etc/apache2/ports.conf   need to add Listen 80  and 8080
+ sed -i 's/INTRAPORT="80"/INTRAPORT="8080"/' /etc/koha/koha-sites.conf
+ sed -i 's/INTRASUFFIX="-intra"/INTRASUFFIX=""/' /etc/koha/koha-sites.conf
+ echo "Listen 8080" >> /etc/apache2/ports.conf
+ 
+ #need to add this to others container using apache2
+ echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+ ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
 
  #need to edit or add file /etc/koha/koha-sites.conf
  a2enmod rewrite
@@ -12,10 +22,7 @@
   #mysql_secure_installation
   mysqladmin -u root password mysqlpsswd
   mysqladmin -u root -pmysqlpsswd reload
-
- #apache2 conf
- #default ports /etc/apache2/ports.conf   need to add Listen 80  and 8080
-
+  
  a2enmod deflate
  a2ensite library
 
@@ -25,4 +32,4 @@
  #koha-rebuild-zebra -v -f library
  
 killall mysqld
-sleep 10s
+sleep 5s
