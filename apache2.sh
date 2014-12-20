a242d55a111ebb474f3138f0530489c3b9a01f1c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-19 21:34:46 -0500

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -3,12 +3,9 @@
 # `/sbin/setuser www-data` runs the given command as the user `www-data`.
 # If you omit that part, the command will be run as root.
 
-set -e
 
 read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
-
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
 source /etc/apache2/envvars
-
-exec apache2ctl -D FOREGROUND >>/var/log/apache2.log 2>&1
+exec /etc/init.d/apache2 start >>/var/log/apache2.log 2>&1
