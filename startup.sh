fd51e7b1620a19ce1666507079dfbb0bb1b3ae49
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-20 09:08:44 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,13 +4,10 @@
 set -e
 
 if [ -f /etc/configured ]; then
-        a2enmod suexec
         echo 'already configured'
 else
       #code that need to run only one time ....
-        a2enmod suexec
-        xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/library/koha-conf.xml
-        #a2enmod suexec
+        echo $(xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/library/koha-conf.xml)
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
