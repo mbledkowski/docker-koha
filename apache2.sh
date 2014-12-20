badb767c47b5e13632137c9a5594ff23dce30258
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-20 10:18:58 -0500

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -8,4 +8,4 @@ read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
 source /etc/apache2/envvars
-exec /etc/init.d/apache2 start >>/var/log/apache2.log 2>&1
+exec apache2ctl -D FOREGROUND >>/var/log/apache2.log 2>&1
