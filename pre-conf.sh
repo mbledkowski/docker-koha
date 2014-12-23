96fd7c721328cebaedb12e60ae64dab1d5fe14e6
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-22 22:00:58 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-/usr/bin/mysqld_safe &
- sleep 5s
-
  #apache2 conf
  #default ports /etc/apache2/ports.conf   need to add Listen 80  and 8080
  sed -i 's/INTRAPORT="80"/INTRAPORT="8080"/' /etc/koha/koha-sites.conf
@@ -12,16 +9,3 @@ set -e
  #need to add this to others container using apache2
  echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
- a2enmod rewrite
- a2enmod suexec
- #need to edit or add file /etc/koha/koha-sites.conf
- a2enmod cgi
- koha-create --create-db library
- #security tweak
-  mysqladmin -u root password mysqlpsswd
-  mysqladmin -u root -pmysqlpsswd reload
- a2dissite 000-default
- rm -R /var/www/html/
-
-killall mysqld
-sleep 5s
