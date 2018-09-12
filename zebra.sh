6cd906802283689b3dc90eb62f1489f12db655e2
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-11 23:26:32 -0400

    Update zebra.sh

--- zebra.sh
+++ zebra.sh
@@ -3,4 +3,4 @@
 # `chpst -u root` runs the given command as the user `root`.
 # If you omit that part, the command will be run as root.
 
-exec chpst -u root koha-start-zebra library 2>&1
+exec chpst -u root koha-zebra --start library 2>&1
