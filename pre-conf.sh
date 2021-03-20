fe73d14b115e6003ce9ad65b733eace13ebd411b
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2021-03-20 15:01:20 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -5,9 +5,9 @@ set -e
  #default ports /etc/apache2/ports.conf   need to add Listen 80  and 8080
  sed -i 's/DOMAIN=".myDNSname.org"/DOMAIN=""/' /etc/koha/koha-sites.conf
  # Testing if I can only used one port for everything
- # sed -i 's/INTRAPORT="80"/INTRAPORT="8080"/' /etc/koha/koha-sites.conf
- # sed -i 's/INTRASUFFIX="-intra"/INTRASUFFIX=""/' /etc/koha/koha-sites.conf
- # echo "Listen 8080" >> /etc/apache2/ports.conf
+ sed -i 's/INTRAPORT="80"/INTRAPORT="8080"/' /etc/koha/koha-sites.conf
+ sed -i 's/INTRASUFFIX="-intra"/INTRASUFFIX=""/' /etc/koha/koha-sites.conf
+ echo "Listen 8080" >> /etc/apache2/ports.conf
  #need to add this to others container using apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf 
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
